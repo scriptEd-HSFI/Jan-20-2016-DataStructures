91e1a0b986f779929c7c3c44788e47a027f567ea
Grace Tan <gtan@wework.com>
2016-01-20 14:33:11 -0500

    Update excercise.js

--- excercise.js
+++ excercise.js
@@ -1,25 +1,11 @@
 //Test input:
-//dontStopPopping([1, 2, 3, 4, 5, 6])
-
-function dontStopPopping(array) {
-  //write code here that removes each element from the array using array.pop
-  //hint: use a for loop! i.e. for(var i = 0; ...)
-
-  //this check will tell you if you've done it right!
-  if (array.length == 0)
-    alert("Yup you got it");
-  else
-    alert("Nah try again :( ");
-}
-
-//Test input:
 //pushMeToTheLimit(5);
 
 function pushMeToTheLimit(arrayMax) {
   var array = [];
 
   //write code here that pushes sequential numbers to an empty array until the length of the array hits the max
-  //hint: another for loop!
+  //hint: use a for loop! i.e. for(var i = 0; ...)
 
   //this check will tell you if you've done it right!
 
@@ -27,6 +13,8 @@ function pushMeToTheLimit(arrayMax) {
     alert("yay");
   else
     alert("merp");
+    
+  return array;
 }
 
 //Test input:
@@ -36,11 +24,28 @@ function findMe(array, searchWord) {
   var indexOfWord;
 
   //write code here that finds the index of the searchWord and sets it to variable indexOfWord
-
+  
   //this check will tell you if you've done it right!
   if (array[indexOfWord] == searchWord)
-    alert("found u")
+    alert("found u");
   else
-    alert(":( nope")
+    alert(":( nope");
+    
+  return indexOfWord;
 }
 
+//Test input:
+//dontStopPopping([1, 2, 3, 4, 5, 6])
+
+function dontStopPopping(array) {
+  //write code here that removes each element from the array using array.pop
+  //hint: another for loop! 
+  
+  //this check will tell you if you've done it right!
+  if (array.length === 0)
+    alert("Yup you got it");
+  else
+    alert("Nah try again :( ");
+    
+  return array;
+}
